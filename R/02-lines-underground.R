fcab5eac03605141b811872b37ba9b50c37e1c64
bydata <ansgar.wolsing@gmail.com>
2023-10-30 20:33:48 +0100

    day 02: lines - Underground London

---
+++ R/02-lines-underground.R
@@ -0,0 +1,171 @@
+library(tidyverse)
+library(sf)
+library(osmdata)
+library(rvest)
+library(here)
+
+#' Download noise pollution data (Rail Noise: Lnight) from:
+#' https://data.london.gov.uk/dataset/noise-pollution-in-london
+#' and unzip the archive
+
+## Get shape of London
+
+shp_city <- getbb("London, United Kingdom", format_out = "sf_polygon", limit = 1)
+st_crs(shp_city)
+st_bbox(shp_city)
+
+# Get Underground features
+underground_features <- opq(bbox = st_bbox(shp_city), timeout = 1200) %>%
+  add_osm_feature(key = "railway", value = "subway") %>%
+  osmdata_sf()
+
+# Get water features
+water_features <- opq(bbox = st_bbox(shp_city), timeout = 1200) %>%
+  add_osm_feature(key = "water", value = "river") %>%
+  osmdata_sf()
+
+
+# Scrape underground lines table from Wikipedia
+
+remove_note_indicator <- function(x) {
+  str_remove_all(x, "\\[.+?\\]")
+}
+
+wiki_url <- "https://en.wikipedia.org/wiki/London_Underground"
+underground_df <- read_html(wiki_url) %>%
+  html_nodes(css = "table.sortable") %>%
+  pluck(1) %>%
+  html_table(header = TRUE) %>%
+  janitor::clean_names() %>%
+  filter(name != "Name") %>%
+  rename(
+    mapcolour = mapcolour_112,
+    length_km = length,
+    length_mi = length_2,
+    average_weekday_ridership_2017 = average_weekday_ridership_2017_113
+  ) %>%
+  mutate(
+    name = str_to_title(str_remove(name, "(?i)\\sLine")),
+    across(everything(), remove_note_indicator),
+    across(c(stations, opened, cars_per_train, average_weekday_ridership_2017,
+             trips_per_year, average_trips_per_mile),
+           function(x) str_remove_all(x, ",") %>% as.integer),
+    across(c(length_km, length_mi), as.numeric)
+  )
+
+# Add line colours RGB codes
+underground_df$mapcolour_rgb <- c("#9A5E34", "#C83C2B", "#F7CF46", "#34783B", "#DD9FAE",
+                         "#7E878D", "#791B53", "#000000", "#0F0698", "#48A1DB",
+                         "#86CCB3")
+
+# Edit names of lines
+unique(underground_features$osm_lines$name)
+underground_df$name
+
+lines_regex <- paste0("(?i)", paste(underground_df$name, collapse = "|"))
+
+# underground_features$osm_lines %>%
+#   transmute(line_name = str_extract(name, lines_regex))
+# underground_features$osm_lines %>%
+#   filter(!is.na(name)) %>%
+#   transmute(
+#     id = row_number(),
+#     name,
+#     line_name = str_extract_all(name, lines_regex)) %>%
+#   unnest(cols = line_name) %>%
+#   View()
+
+underground_features_lines <- underground_features$osm_lines %>%
+  filter(!is.na(name)) %>%
+  mutate(
+    row_id = row_number(),
+    line_name = str_extract_all(name, lines_regex)) %>%
+  unnest(cols = line_name) %>%
+  mutate(line_name = str_to_title(line_name)) %>%
+  select(row_id, osm_id, name, line_name, geometry) %>%
+  inner_join(underground_df, by = join_by(line_name == name))
+
+# # Move the District Line slightly North
+# shift_north_value <- 0.01
+# underground_features_lines$geometry[underground_features_lines$name == "District Line"] <-
+#   underground_features_lines$geometry[underground_features_lines$name == "District Line"] +
+#   shift_north_value
+
+bg_color <- "grey13"
+
+
+geom_line_outer_glow <- function(x) {
+
+  df <- ~subset(., line_name == x)
+  # colour <- unique(df$mapcolour_rgb)[1]
+  colour <- "blue"
+
+  ggfx::with_outer_glow(
+    geom_sf(
+      data = df,
+      color = colour,
+      linewidth = 0.4
+    ),
+    expand = 8, sigma = 4,
+    colour = colorspace::lighten(colour, 0.6)
+  )
+}
+
+ragg::agg_png(here("plots", "02-lines-with-glow.png"), width = 8, height = 5,
+              res = 600, units = "in")
+underground_features_lines %>%
+  ggplot() +
+  geom_sf(
+    data = shp_city,
+    fill = "grey20", linewidth = 0
+  ) +
+  geom_sf(
+    data = water_features$osm_polygons,
+    fill = bg_color, linewidth = 0
+  ) +
+  geom_sf(
+    data = water_features$osm_multipolygons,
+    fill = bg_color, linewidth = 0
+  ) +
+  # ggfx::with_outer_glow(
+  #   geom_sf(
+  #     aes(color = line_name),
+  #     linewidth = 0.8
+  #   ),
+  #   expand = 8, colour = "white", sigma = 4
+  # ) +
+  ggfx::with_outer_glow(
+    geom_sf(
+      data = ~subset(., line_name == "District"),
+      color = "#34783B",
+      linewidth = 0.4
+    ),
+    expand = 9, colour = colorspace::lighten("#34783B", 0.8), sigma = 2
+  ) +
+  # ggfx::with_outer_glow(
+  #   geom_sf(
+  #     data = ~subset(., line_name == "Victoria Line"),
+  #     color = "#48A1DB",
+  #     linewidth = 0.4
+  #   ),
+  #   expand = 9, sigma = 2,
+  #   colour = colorspace::lighten("#48A1DB", 0.8)
+  # ) +
+  # geom_line_outer_glow("Victoria Line", "#48A1DB") +
+  geom_line_outer_glow("Victoria") +
+  ggfx::with_outer_glow(
+    geom_sf(
+      data = ~subset(., line_name == "Northern"),
+      color = "#000000",
+      linewidth = 0.4
+    ),
+    expand = 9, colour = colorspace::lighten("#000000", 0.8), sigma = 2
+  ) +
+  scale_color_manual(values = underground_df$mapcolour_rgb) +
+  theme_void() +
+  theme(
+    plot.background = element_rect(color = bg_color, fill = bg_color),
+    text = element_text(color = "grey80")
+  )
+dev.off()
+
