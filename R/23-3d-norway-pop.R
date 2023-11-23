70f1b307f5acfa1f1a3de903eac652049ee588b5
bydata <ansgar.wolsing@gmail.com>
2023-11-23 17:59:21 +0100

    day 23 - 3D - Norway population density

--- R/23-3d-norway-pop.R
+++ R/23-3d-norway-pop.R
@@ -4,9 +4,9 @@ library(sf)
 library(tidyverse)
 library(stars)
 library(rayshader)
-library(MetBrewer)
-library(colorspace)
 library(here)
+
+# Transform population data for height map -------------------------------------
 
 target_crs <- "EPSG:25834"
 
@@ -19,7 +19,6 @@ st_crs(kontur)
 bb <- st_bbox(kontur)
 bb["xmin"] <- 2e5
 bb["ymax"] <- 12e6
-bb
 
 kontur <- kontur %>%
   st_filter(st_as_sfc(st_bbox(bb, crs = st_crs(kontur)))) %>%
@@ -33,18 +32,8 @@ bottom_right <- st_point(c(bb[["xmax"]], bb[["ymin"]]))  %>%
   st_sfc(crs = st_crs(kontur))
 top_left <- st_point(c(bb[["xmin"]], bb[["ymax"]]))  %>%
   st_sfc(crs = st_crs(kontur))
-
 width <- st_distance(bottom_left, bottom_right)
 height <- st_distance(bottom_left, top_left)
-
-# check by plotting points
-kontur  %>%
-  ggplot() +
-  geom_sf() +
-  geom_sf(data = bottom_left) +
-  geom_sf(data = bottom_right, color = "red") +
-  geom_sf(data = top_left, color = "green")
-
 
 # handle conditions of width or height being the longer side
 if (width > height) {
@@ -56,25 +45,22 @@ if (width > height) {
 }
 
 # convert to raster so we can then convert to matrix
-
 size <- 5000
 rast <- st_rasterize(kontur, nx = floor(size * w_ratio), ny = floor(size * h_ratio))
-
 mat <- matrix(rast$population,
               nrow = floor(size * w_ratio),
               ncol = floor(size * h_ratio))
 
-# create color palette
+
+# Render the spike map ---------------------------------------------------------
 
 # Custom texture on flag colours
-lightcolor <- "#FFFFFF"
-shadowcolor <- "#00205B"
-leftcolor <- "#EF3340"
-rightcolor <- "#EF3340"
-centercolor <- "#EF3340"
-lightcolor2 <- lighten(leftcolor, 0.75)
+white <- "#FFFFFF"
+darkblue <- "#00205B"
+red <- "#EF3340"
+lightred <- lighten(leftcolor, 0.75)
 
-custom_texture_pal <- colorRampPalette(colors = c(shadowcolor, lightcolor, rep(leftcolor, 3)))
+custom_texture_pal <- colorRampPalette(colors = c(darkblue, white, rep(red, 3)))
 swatchplot(custom_texture_pal(128))
 texture <- custom_texture_pal(128)
 
@@ -86,8 +72,8 @@ mat  %>%
   plot_3d(heightmap = mat,
           windowsize = c(600, 600),
           zoom = 0.5,
-          theta = -11.5,
-          phi = 28.5,
+          theta = -19.5,
+          phi = 22.75,
           zscale = 11,
           background = "white",
           solid = FALSE,
@@ -97,8 +83,10 @@ mat  %>%
 render_camera()
 # theta       phi      zoom       fov
 # -12.73399  19.43781   0.50000   0.00000
+# theta       phi      zoom       fov
+# -19.48477  22.75316   0.50000   0.00000
 
-outfile <- here("plots", "23-3d-norway-pop-hi-res-04.png")
+outfile <- here("plots", "23-3d-norway-pop-hi-res-10.png")
 
 {
   start_time <- Sys.time()
@@ -109,10 +97,10 @@ outfile <- here("plots", "23-3d-norway-pop-hi-res-04.png")
   render_highquality(
     filename = outfile,
     interactive = FALSE,
-    lightdirection = 280,
+    lightdirection = 240,
     lightaltitude = c(20, 80),
-    lightcolor = c(lightcolor2, "white"),
-    lightintensity = c(600, 100),
+    lightcolor = c(lightred, "white"),
+    lightintensity = c(500, 100),
     samples = 450,
     width = 6000,
     height = 6000,
