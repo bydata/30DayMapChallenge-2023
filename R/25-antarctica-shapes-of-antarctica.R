ecef77aa807714aa7f99c3488e124319fe26d7b9
bydata <ansgar.wolsing@gmail.com>
2023-11-25 16:07:58 +0100

    rename script; add plot function and plot several projections

--- R/25-antarctica-shapes-of-antarctica.R
+++ R/25-antarctica-shapes-of-antarctica.R
@@ -1,31 +1,30 @@
 library(tidyverse)
 library(sf)
 library(patchwork)
+library(elevatr)
 library(here)
 
-
-shape <- giscoR::gisco_get_countries(country = "Antarctica")
+# Get shape
+shp <- giscoR::gisco_get_countries(country = "Antarctica", resolution = "03")
+st_bbox(shp)
 
 # Which CRS is it?
-st_crs(shape)
+st_crs(shp)
 
 # Function to plot the shape
-plot_proj <- function(crs = st_crs(shape), name) {
-  shape %>%
+plot_proj <- function(crs = st_crs(shp), name) {
+  shp %>%
     st_transform(crs = crs) %>%
     ggplot() +
-    geom_sf(color = NA, fill = "#F2F7F2") +
-    labs(title = name) +
-    theme_void(base_family = "Outfit") +
-    theme(
-      plot.background = element_rect(color = "#443850", fill = "#443850"),
-      panel.background = element_rect(color = NA, fill = NA),
-      plot.title = element_text(color = "#F2F7F2", hjust = 0.5)
-    )
+    geom_sf(fill = NA, color = "#F2F7F2", linewidth = 0.1) +
+    coord_sf(default_crs = NULL, lims_method = "geometry_bbox") +
+    # labs(title = name) +
+    theme_void()
 }
 
 # https://proj.org/en/9.3/operations/projections/mbtfpp.html
 
+# WGS84
 (p1 <- plot_proj("EPSG:4326", "EPSG:4326"))
 # plot_proj("WGS84", "WGS")
 
@@ -59,7 +58,6 @@ plot_proj <- function(crs = st_crs(shape), name) {
 #' https://proj.org/en/9.3/operations/projections/vitk1.html
 (p9 <- plot_proj("+proj=vitk1 +lat_1=45 +lat_2=55", "Vitkovsky I"))
 
-
 #' Rectangular Polyconic
 #' https://proj.org/en/9.3/operations/projections/rpoly.html
 (p10 <- plot_proj("+proj=rpoly", "Rectangular Polyconic"))
@@ -68,17 +66,21 @@ plot_proj <- function(crs = st_crs(shape), name) {
 #' https://proj.org/en/9.3/operations/projections/vandg.html
 (p11 <- plot_proj("+proj=vandg", "van der Grinten"))
 
-#'
-#'
-plot_proj("")
+#' Stereographic
+#' https://proj.org/en/9.3/operations/projections/stere.html
+(p12 <- plot_proj("+proj=stere +lat_0=-90", "Stereographic"))
+
+#' Azimuthal Equidistant
+#' https://proj.org/en/9.3/operations/projections/aeqd.html
+(p13 <- plot_proj("+proj=aeqd +lat_0=-90", "Azimuthal Equidistant"))
+
+#' Adams World in a Square I
+#' https://proj.org/en/9.3/operations/projections/adams_ws1.html
+(p14 <- plot_proj("+proj=adams_ws1"))
 
 #'
 #'
-plot_proj("")
-
-#'
-#'
-plot_proj("")
+# plot_proj("")
 
 layout <- "
 112233
@@ -86,31 +88,31 @@ layout <- "
 778899
 "
 
-p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 +
-  plot_layout(design = layout) &
-  theme(
-    plot.background = element_rect(color = "#443850", fill = "#443850")
-  )
+# p1 + p2 + p3 + p4 + p5 + p6 + p7 + p8 + p9 + p10 +
+#   plot_layout(design = layout) &
+#   theme(
+#     plot.background = element_rect(color = "#443850", fill = "#443850")
+#   )
+#
 
-plot_width <- 2.5
-plot_height <- 2
+## Combine plots using magick ==================================================
+
+plot_width <- 1
+plot_height <- 1
 plot_path <- here("plots", "25-antarctica-plots")
 
 ggsave(here(plot_path, "p1.png"), plot = p1, width = plot_width, height = plot_height)
 ggsave(here(plot_path, "p2.png"), plot = p2, width = plot_width, height = plot_height)
 ggsave(here(plot_path, "p3.png"), plot = p3, width = plot_width, height = plot_height)
 ggsave(here(plot_path, "p4.png"), plot = p4, width = plot_width, height = plot_height)
+ggsave(here(plot_path, "p5.png"), plot = p5, width = plot_width, height = plot_height)
+ggsave(here(plot_path, "p6.png"), plot = p6, width = plot_width, height = plot_height)
+ggsave(here(plot_path, "p7.png"), plot = p7, width = plot_width, height = plot_height)
+ggsave(here(plot_path, "p8.png"), plot = p8, width = plot_width, height = plot_height)
+ggsave(here(plot_path, "p9.png"), plot = p9, width = plot_width, height = plot_height)
+ggsave(here(plot_path, "p10.png"), plot = p10, width = plot_width, height = plot_height)
+ggsave(here(plot_path, "p11.png"), plot = p11, width = plot_width, height = plot_height)
+ggsave(here(plot_path, "p12.png"), plot = p12, width = plot_width, height = plot_height)
+ggsave(here(plot_path, "p13.png"), plot = p13, width = plot_width, height = plot_height)
+ggsave(here(plot_path, "p14.png"), plot = p14, width = plot_width, height = plot_height)
 
-library(magick)
-
-img1 <- image_read(here(plot_path, "p1.png"))
-img2 <- image_read(here(plot_path, "p2.png"))
-img3 <- image_read(here(plot_path, "p3.png"))
-img4 <- image_read(here(plot_path, "p4.png"))
-
-img1_2 <- image_append(
-  c(img1, img2), stack = FALSE)
-img3_4 <- image_append(
-  c(img3, img4), stack = FALSE)
-combined <- image_append(c(img1_2, img3_4), stack = TRUE)
-image_write(combined, here(plot_path, "combined.png"))
